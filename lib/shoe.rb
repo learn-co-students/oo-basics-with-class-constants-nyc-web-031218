8e5d5fd99b245d6800e63b6cd8358195e70616f1
github username <github email address>
2018-03-16 12:30:44 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
   end
@@ -11,4 +13,20 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand= (brand)
+    @brand = brand
+    BRANDS << brand
+
 end
+
+# nike = Shoe.new("Nike")
+# nike.color = "blue"
+# nike.size = 10
+# nike.material = "leather"
+# nike.condition = "old"
+# puts nike.brand
+# puts nike.color
+# puts nike.size
+# puts nike.material
+# puts nike.condition
+# nike.cobble
